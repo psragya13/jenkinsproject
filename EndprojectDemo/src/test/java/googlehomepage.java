5d61f9dd8bbd9dda2544db317160979f0b89fec2
Prachi Sharma <pacificprachi@gmail.com>
2022-04-26 22:47:48 +0530

    Update googlehomepage.java
    adding comment

--- EndprojectDemo/src/test/java/googlehomepage.java
+++ EndprojectDemo/src/test/java/googlehomepage.java
@@ -13,5 +13,6 @@ public class googlehomepage {
 	System.setProperty("webdriver.chrome.driver","C://Users//prachi.sharma1//Downloads//chromedriver_win32//chromedriver.exe");
 	  WebDriver driver= new ChromeDriver(); 
 	    driver.get("https://www.saucedemo.com/");
+		//commenting
 }
 }
