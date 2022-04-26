255716c56ea07d9c1d1cda4981619abcbe01775b
Prachi Sharma <pacificprachi@gmail.com>
2022-04-26 22:43:17 +0530

    Add files via upload

---
+++ EndprojectDemo/src/test/java/googlehomepage.java
@@ -0,0 +1,17 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.WebElement;
+
+public class googlehomepage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	
+	System.setProperty("webdriver.chrome.driver","C://Users//prachi.sharma1//Downloads//chromedriver_win32//chromedriver.exe");
+	  WebDriver driver= new ChromeDriver(); 
+	    driver.get("https://www.saucedemo.com/");
+}
+}
